eaa4c1962be3e6d5a668ba8700e24ca7c02285f7
Ben <benw@student.chalmers.se>
2020-12-03 22:42:12 +0100

    Day 1

---
+++ Day 1/main.py
@@ -0,0 +1,23 @@
+with open("nums.txt") as f:
+    lines = [int(x) for x in f.readlines()]
+
+
+def part1():
+    for x in lines:
+        for y in lines:
+            if x+y == 2020:
+                print(x, " + ", y, " = ", x*y)
+                return
+
+
+def part2():
+    for x in lines:
+        for y in lines:
+            for z in lines:
+                if x + y + z == 2020:
+                    print(x, " + ", y, " + ", z, " = ", x * y * z)
+                    return
+
+if __name__ == '__main__':
+    part1()
+    part2()
